ed777f0b1ff5baaf1dec3e6354e0e33b91f5e88b
Adam Presley <adam@adampresley.com>
2017-06-29 10:34:04 -0500

    Started on the AST

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -28,7 +28,15 @@ func (l *Lexer) readChar() {
 	}
 
 	l.position = l.readPosition
-	l.readPosition += 1
+	l.readPosition++
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	}
+
+	return l.input[l.readPosition]
 }
 
 func (l *Lexer) NextToken() token.Token {
@@ -38,7 +46,15 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
@@ -54,6 +70,32 @@ func (l *Lexer) NextToken() token.Token {
 
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
+
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+
+	case '<':
+		tok = newToken(token.LT, l.ch)
+
+	case '>':
+		tok = newToken(token.GT, l.ch)
 
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
